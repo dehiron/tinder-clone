9957804844615fd925b06bf039b48e53e77bcf81
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-01-17 17:04:36 +0900

    Build the Header Section on the HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,35 @@
+import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Button, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { Image, Button, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import tailwind from "tailwind-rn";
 import useAuth from "../hooks/useAuth";
 
 const HomeScreen = () => {
 
     const navigation = useNavigation();
-    const { logout } = useAuth();
+    const { user, logout } = useAuth();
+    console.log(user)
 
     return(
-        <View>
-            <Text>I am homescreen</Text>
+        <SafeAreaView>
+            {/* Header */}
+            <View style={tailwind("flex-row justify-between items-center px-5")}>
+                <TouchableOpacity onPress={logout}>
+                    <Image style={tailwind("h-10 w-10 rounded-full")} source={{ uri:user.photoURL}} />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image style={tailwind("h-14 w-14")} source={require("../assets/favicon.png")} />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+                    <Ionicons name = "chatbubbles-sharp" size={30} />
+                </TouchableOpacity>
+            </View>
+            {/* End of header */}
+
+            {/* <Text>I am homescreen</Text>
             <Button 
                 title="Gp to Chat Screen" 
                 onPress={() => navigation.navigate("Chat")}
@@ -18,8 +37,10 @@ const HomeScreen = () => {
             <Button  
                 title="Logout" 
                 onPress={ logout } 
-            />
-        </View>
+            /> */}
+
+
+        </SafeAreaView>
     )
 }
 
