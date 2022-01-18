26e330adccb33f7ae9dd2f70c330551e865defcc
Hidehiro Aya <hi.dehiro424@gmail.com>
2022-01-19 00:09:04 +0900

    Apply keyboardAvoidingView on mdoal screen

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,7 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import React, { useLayoutEffect, useState } from "react";
-import { TouchableOpacity, Text, View, Image, TextInput } from "react-native";
+import { TouchableOpacity, Text, View, Image, TextInput, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native";
 import tailwind from "tailwind-rn";
 import { db } from "../firebase";
 import useAuth from "../hooks/useAuth";
@@ -44,66 +44,76 @@ const ModalScreen = () => {
     };
 
     return (
-        <View style={tailwind("flex-1 items-center pt-1")}>
-            <Image 
-                style={tailwind("h-20 w-full")}
-                resizeMode="contain"
-                source={{ uri: "https://links.papareact.com/2pf"}}
-            />
 
-            <Text style={tailwind("text-xl text-gray-500 p-2 font-bold")} >
-                Welcome {user.displayName}
-            </Text>
+        <KeyboardAvoidingView style={tailwind("flex-1")}>
+            <ScrollView>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={tailwind("flex-1 items-center pt-1")}>
+                        <Image 
+                            style={tailwind("h-20 w-full")}
+                            resizeMode="contain"
+                            source={{ uri: "https://links.papareact.com/2pf"}}
+                        />
 
-            <Text style={tailwind("text-center p-4 font-bold text-red-400")} >
-                Step 1: The Profile Pic
-            </Text>
-            <TextInput
-                value={image}
-                onChangeText={(text) => setImage(text)}
-            
-                style={tailwind("text-center text-xl pb-2")}
-                placeholder="Enter a Profile Pic URL"
-            />
+                        <Text style={tailwind("text-xl text-gray-500 p-2 font-bold")} >
+                            Welcome {user.displayName}
+                        </Text>
 
-            <Text style={tailwind("text-center p-4 font-bold text-red-400")} >
-                Step 2: The Job
-            </Text>
-            <TextInput
-                value={job}
-                onChangeText={(text) => setJob(text)}
-                style={tailwind("text-center text-xl pb-2")}
-                placeholder="Enter your occupation"
-            />
+                        <Text style={tailwind("text-center p-4 font-bold text-red-400")} >
+                            Step 1: The Profile Pic
+                        </Text>
+                        <TextInput
+                            value={image}
+                            onChangeText={(text) => setImage(text)}
+                        
+                            style={tailwind("text-center text-xl pb-2")}
+                            placeholder="Enter a Profile Pic URL"
+                        />
 
-            <Text style={tailwind("text-center p-4 font-bold text-red-400")} >
-                Step 1: The Age
-            </Text>
-            <TextInput
-                value={age}
-                onChangeText={(text) => setAge(text)}
-                style={tailwind("text-center text-xl pb-2")}
-                placeholder="Enter your age"
-                keyboardType="numeric"
-                maxLength={2}
-            />
+                        <Text style={tailwind("text-center p-4 font-bold text-red-400")} >
+                            Step 2: The Job
+                        </Text>
+                        <TextInput
+                            value={job}
+                            onChangeText={(text) => setJob(text)}
+                            style={tailwind("text-center text-xl pb-2")}
+                            placeholder="Enter your occupation"
+                        />
 
-            <TouchableOpacity 
-                disabled={incompleteForm}
-                style={[
-                    tailwind("w-64 p-3 rounded-xl absolute bottom-10 bg-red-400"),
-                    incompleteForm ? tailwind("bg-gray-400") : tailwind("bg-red-400"),
-                ]}
-                onPress={updateUserProfile}
-            >
-                <Text 
-                    style={tailwind("text-center text-white text-xl")}
+                        <Text style={tailwind("text-center p-4 font-bold text-red-400")} >
+                            Step 1: The Age
+                        </Text>
+                        <TextInput
+                            value={age}
+                            onChangeText={(text) => setAge(text)}
+                            style={tailwind("text-center text-xl pb-2")}
+                            placeholder="Enter your age"
+                            keyboardType="numeric"
+                            maxLength={2}
+                        />
+
+                    </View>
+                </TouchableWithoutFeedback>
+            </ScrollView>
+
+            <View style={tailwind("flex-1 items-center pt-1")}>
+                <TouchableOpacity 
+                    disabled={incompleteForm}
+                    style={[
+                        tailwind("w-64 p-3 rounded-xl absolute bottom-10 bg-red-400"),
+                        incompleteForm ? tailwind("bg-gray-400") : tailwind("bg-red-400"),
+                    ]}
+                    onPress={updateUserProfile}
                 >
-                    Update Profile
-                </Text>
-            </TouchableOpacity>
+                    <Text 
+                        style={tailwind("text-center text-white text-xl")}
+                    >
+                        Update Profile
+                    </Text>
+                </TouchableOpacity>
+            </View>
 
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
